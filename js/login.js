fcf0d6bd030dcc13fbb055308c007cc20137afb4
Aman Singh <aman.singh@student.kdg.be>
2023-05-13 22:31:40 +0200

    added name of the user in the legend when login is done

--- js/login.js
+++ js/login.js
@@ -1,32 +1,32 @@
-window.onload = function () {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function (event) {
-        event.preventDefault(); // prevent form submission
+const form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent form submission
 
-        const usernameInput = document.querySelector('#username');
-        const passwordInput = document.querySelector('#password');
-        const username = usernameInput.value;
-        const password = passwordInput.value;
+    const usernameInput = document.querySelector('#username');
+    const passwordInput = document.querySelector('#password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 
-        // Check if the username and password are not empty
-        if (!username || !password) {
-            alert('Please enter both username and password.');
-            return;
-        }
+    // Check if the username and password are not empty
+    if (!username || !password) {
+        alert('Please enter both username and password.');
+        return;
+    }
 
-        // Check if the password is the reverse of the username
-        if (password !== username.split('').reverse().join('')) {
-            alert('Incorrect password. Please try again.');
-            passwordInput.classList.remove('correct');
-            passwordInput.classList.add('incorrect');
-            console.log("error");
-        }
-        // Login successful, redirect to a home page
-        else {
-            alert('Login successful!');
-            passwordInput.classList.remove('incorrect');
-            passwordInput.classList.add('correct');
-            // window.location.href= '../index.html';
-        }
-    });
-}
+    // Check if the password is the reverse of the username
+    if (password !== username.split('').reverse().join('')) {
+        alert('Incorrect password. Please try again.');
+        passwordInput.classList.remove('correct');
+        passwordInput.classList.add('incorrect');
+        console.log("error");
+    }
+    // Login successful, update the legend h2 with the username
+    else {
+        const legend = document.querySelector('legend h2');
+        legend.textContent = `WELCOME ${username}`.toUpperCase();
+        alert('Login successful!');
+        passwordInput.classList.remove('incorrect');
+        passwordInput.classList.add('correct');
+        // window.location.href= '../index.html';
+    }
+});
