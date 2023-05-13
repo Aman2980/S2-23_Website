c34f45284bbd6538be71b87e516a76e7eefdfd29
Aman Singh <aman.singh@student.kdg.be>
2023-05-13 22:47:46 +0200

    Form validation complete

--- js/login.js
+++ js/login.js
@@ -15,18 +15,25 @@ form.addEventListener('submit', function (event) {
 
     // Check if the password is the reverse of the username
     if (password !== username.split('').reverse().join('')) {
-        alert('Incorrect password. Please try again.');
+        // alert('Incorrect password. Please try again.');
         passwordInput.classList.remove('correct');
         passwordInput.classList.add('incorrect');
+        const errorMessage = document.querySelector('.errorMessage')
+        errorMessage.innerHTML = ('Incorrect password');
+        errorMessage.style.color=('white');
         console.log("error");
     }
     // Login successful, update the legend h2 with the username
     else {
         const legend = document.querySelector('legend h2');
         legend.textContent = `WELCOME ${username}`.toUpperCase();
-        alert('Login successful!');
+        // alert('Login successful!');
         passwordInput.classList.remove('incorrect');
         passwordInput.classList.add('correct');
+        const errorMessage = document.querySelector('.errorMessage')
+        if (errorMessage) {
+            errorMessage.remove();
+        }
         // window.location.href= '../index.html';
     }
 });
