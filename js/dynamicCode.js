42bacdac40b42979b73a35e2d3828d36b9d3731f
Aman Singh <aman.singh@student.kdg.be>
2023-05-14 19:19:56 +0200

    Refactored all the js code into a bunch of functions

--- js/dynamicCode.js
+++ js/dynamicCode.js
@@ -1,50 +1,51 @@
-window.onload = function () {
-    document.addEventListener('DOMContentLoaded', function() {
-        // responsive navbar
-        const hamburgerMenu = document.querySelector(".hamburger");
-        const navMenu = document.querySelector(".nav-menu");
+function addResponsiveNavbar() {
+    const hamburgerMenu = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
 
+    hamburgerMenu.addEventListener("click", () => {
+        hamburgerMenu.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+}
 
-        hamburgerMenu.addEventListener("click", () => {
-            hamburgerMenu.classList.toggle("active");
-            navMenu.classList.toggle("active");
-        });
+function addButtonSoundAnimation() {
+    const buttons = document.querySelectorAll('.download-btn');
+    const buttonSound = document.querySelector('#buttonSound');
 
-        // document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-        //     hamburgerMenu.classList.remove("active");
-        //     navMenu.classList.remove("active");
-        // }));
-
-
-        // button sound animation
-        const buttons = document.querySelectorAll('.download-btn');
-        const buttonSound = document.querySelector('#buttonSound');
-
-        buttons.forEach(button => {
-            button.addEventListener('click', () => {
-                buttonSound.currentTime = 0;
-                buttonSound.play();
-            });
-        });
-
-        // gameVideo
-        const watchTrailerBtn = document.querySelector('.heroBtn:nth-child(2)');
-        const gameVideo = document.getElementById('game-video');
-        const gameVideoSection = document.querySelector('.gameVideo');
-
-        watchTrailerBtn.addEventListener('click', function () {
-            if (gameVideoSection.style.display === 'none') {
-                gameVideoSection.style.display = 'block';
-                gameVideo.play();
-            } else {
-                gameVideoSection.style.display = 'none';
-                gameVideo.pause();
-            }
-        });
-
-        const learnMoreBtn = document.querySelector('.heroBtn');
-        learnMoreBtn.addEventListener('click', function () {
-            window.location.href = 'game.html';
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttonSound.currentTime = 0;
+            buttonSound.play();
         });
     });
-};
+}
+
+function addGameVideoHandlers() {
+    const watchTrailerBtn = document.querySelector('.heroBtn:nth-child(2)');
+    const gameVideo = document.getElementById('game-video');
+    const gameVideoSection = document.querySelector('.gameVideo');
+
+    watchTrailerBtn.addEventListener('click', function () {
+        if (gameVideoSection.style.display === 'none') {
+            gameVideoSection.style.display = 'block';
+            gameVideo.play();
+        } else {
+            gameVideoSection.style.display = 'none';
+            gameVideo.pause();
+        }
+    });
+}
+
+function addLearnMoreHandler() {
+    const learnMoreBtn = document.querySelector('.heroBtn');
+    learnMoreBtn.addEventListener('click', function () {
+        window.location.href = 'game.html';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    addResponsiveNavbar();
+    addButtonSoundAnimation();
+    addGameVideoHandlers();
+    addLearnMoreHandler();
+});
--- js/login.js
+++ js/login.js
@@ -1,51 +1,78 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // prevent form submission
-        const usernameInput = document.querySelector('#username');
-        const passwordInput = document.querySelector('#password');
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-
-        // Check if the username and password are not empty
-        if (!username || !password) {
-            alert('Please enter both username and password.');
-            return;
-        }
-
-        // Check if the password is the reverse of the username
-        if (password !== username.split('').reverse().join('')) {
-            passwordInput.classList.remove('correct');
-            passwordInput.classList.add('incorrect');
-            const errorMessage = document.querySelector('.errorMessage')
-            errorMessage.innerHTML = ('Incorrect password');
-            errorMessage.style.color = ('red');
-            errorMessage.style.marginTop = ('5px');
-            console.log("error");
-        } else {
-            const legend = document.querySelector('legend h2');
-            if (legend) {
-                legend.textContent = `WELCOME ${username}`.toUpperCase();
-            }
-            passwordInput.classList.remove('incorrect');
-            passwordInput.classList.add('correct');
-            localStorage.setItem('username', username);
-            // const loggedInUsername = localStorage.getItem('username');
-            // if (loggedInUsername) {
-            //     const usernameSpan = document.querySelector('.username');
-            //     usernameSpan.textContent = `Welcome ${loggedInUsername}`.toUpperCase();
-            // }
-            const errorMessage = document.querySelector('.errorMessage')
-            if (errorMessage) {
-                errorMessage.remove();
-            }
-            const loading = document.querySelector('.wave')
-            loading.style.visibility = 'visible';
-            setTimeout(() => {
-                loading.style.visibility = 'hidden';
-                window.location.href = '../html/Stats.html';
-            }, 2000);
-        }
-    });
+    form.addEventListener('submit', handleSubmit);
 });
+
+function handleSubmit(event) {
+    event.preventDefault(); // prevent form submission
+    const usernameInput = document.querySelector('#username');
+    const passwordInput = document.querySelector('#password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // Check if the username and password are not empty
+    if (!username || !password) {
+        alertUser('Please enter both username and password.');
+        return;
+    }
+
+    // Check if the password is the reverse of the username
+    if (password !== reverseString(username)) {
+        displayPasswordError();
+    } else {
+        handleSuccessfulLogin(username, passwordInput);
+    }
+}
+
+function alertUser(message) {
+    alert(message);
+}
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function displayPasswordError() {
+    const passwordInput = document.querySelector('#password');
+    passwordInput.classList.remove('correct');
+    passwordInput.classList.add('incorrect');
+    const errorMessage = document.querySelector('.errorMessage');
+    errorMessage.innerHTML = 'Incorrect password';
+    errorMessage.style.color = 'red';
+    errorMessage.style.marginTop = '5px';
+    console.log('error');
+}
+
+function handleSuccessfulLogin(username, passwordInput) {
+    const legend = document.querySelector('legend h2');
+    if (legend) {
+        legend.textContent = `WELCOME ${username}`.toUpperCase();
+    }
+    passwordInput.classList.remove('incorrect');
+    passwordInput.classList.add('correct');
+    localStorage.setItem('username', username);
+    removeErrorMessage();
+    showLoadingIndicator();
+    setTimeout(() => {
+        hideLoadingIndicator();
+        window.location.href = '../html/Stats.html';
+    }, 2000);
+}
+
+function removeErrorMessage() {
+    const errorMessage = document.querySelector('.errorMessage');
+    if (errorMessage) {
+        errorMessage.remove();
+    }
+}
+
+function showLoadingIndicator() {
+    const loading = document.querySelector('.wave');
+    loading.style.visibility = 'visible';
+}
+
+function hideLoadingIndicator() {
+    const loading = document.querySelector('.wave');
+    loading.style.visibility = 'hidden';
+}
