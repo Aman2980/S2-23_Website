a88bdc6a0ba044b11badf76c79e3a020a67854a2
Aman Singh <aman.singh@student.kdg.be>
2023-05-14 15:50:44 +0200

    Something is up and idk what the fuck is going on

--- js/login.js
+++ js/login.js
@@ -1,63 +1,50 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', function (event) {
-    event.preventDefault(); // prevent form submission
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // prevent form submission
+        const usernameInput = document.querySelector('#username');
+        const passwordInput = document.querySelector('#password');
+        const username = usernameInput.value;
+        const password = passwordInput.value;
 
-    const usernameInput = document.querySelector('#username');
-    const passwordInput = document.querySelector('#password');
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-
-    // Check if the username and password are not empty
-    if (!username || !password) {
-        alert('Please enter both username and password.');
-        return;
-    }
-
-    // Check if the password is the reverse of the username
-    if (password !== username.split('').reverse().join('')) {
-        // alert('Incorrect password. Please try again.');
-        passwordInput.classList.remove('correct');
-        passwordInput.classList.add('incorrect');
-        const errorMessage = document.querySelector('.errorMessage')
-        errorMessage.innerHTML = ('Incorrect password');
-        errorMessage.style.color = ('red');
-        errorMessage.style.marginTop = ('5px');
-        console.log("error");
-    }
-    // Login successful, update the legend h2 with the username
-    else {
-        const legend = document.querySelector('legend h2');
-        legend.textContent = `WELCOME ${username}`.toUpperCase();
-// alert('Login successful!');
-        passwordInput.classList.remove('incorrect');
-        passwordInput.classList.add('correct');
-        // Store the username in session storage
-        sessionStorage.setItem('username', username);
-
-        const errorMessage = document.querySelector('.errorMessage')
-        if (errorMessage) {
-            errorMessage.remove();
-        }
-        const loading = document.querySelector('.wave')
-        loading.style.visibility = 'visible';
-        setTimeout(() => {
-            loading.style.visibility = 'hidden';
-            window.location.href = '../html/Stats.html';
-        }, 2000);
-        // const navLink = document.querySelector('.nav-link:nth-child(4)');
-        // navLink.textContent = 'Logout';
-
-
-        // Retrieve the username from session storage
-        const loggedInUsername = sessionStorage.getItem('username');
-
-        // If the username exists, display it in the greeting
-        if (loggedInUsername) {
-            const usernameSpan = document.querySelector('#username');
-            usernameSpan.textContent = username;
+        // Check if the username and password are not empty
+        if (!username || !password) {
+            alert('Please enter both username and password.');
+            return;
         }
 
-    }
+        // Check if the password is the reverse of the username
+        if (password !== username.split('').reverse().join('')) {
+            passwordInput.classList.remove('correct');
+            passwordInput.classList.add('incorrect');
+            const errorMessage = document.querySelector('.errorMessage')
+            errorMessage.innerHTML = ('Incorrect password');
+            errorMessage.style.color = ('red');
+            errorMessage.style.marginTop = ('5px');
+            console.log("error");
+        } else {
+            const legend = document.querySelector('legend h2');
+            if (legend) {
+                legend.textContent = `WELCOME ${username}`.toUpperCase();
+            }
+            passwordInput.classList.remove('incorrect');
+            passwordInput.classList.add('correct');
+            localStorage.setItem('username', username);
+            const loggedInUsername = localStorage.getItem('username');
+            if (loggedInUsername) {
+                const usernameSpan = document.querySelector('.username');
+                usernameSpan.textContent = `Welcome ${loggedInUsername}`.toUpperCase();
+            }
+            const errorMessage = document.querySelector('.errorMessage')
+            if (errorMessage) {
+                errorMessage.remove();
+            }
+            const loading = document.querySelector('.wave')
+            loading.style.visibility = 'visible';
+            setTimeout(() => {
+                loading.style.visibility = 'hidden';
+                window.location.href = '../html/Stats.html';
+            }, 2000);
+        }
+    });
 });
-
-
