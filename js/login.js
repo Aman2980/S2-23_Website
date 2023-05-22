38e515a7662511560df14b11ac5b15a3a04486a3
Aman Singh <aman.singh@student.kdg.be>
2023-05-22 13:00:00 +0200

    made the website more responsive, changed margin top from % to px

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 import {gamesJson} from "../db/db.js";
 
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
     form.addEventListener('submit', handleSubmit);
